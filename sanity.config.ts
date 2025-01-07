aa9b9dd4cd561a13a1a02968abc5a15d03641496
Y Do Thien <y.dothien@story.co>
2025-01-07 20:30:54 +0700

    feat: add documents pane plugin and update structure for default document views

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,9 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+import { structure } from './structure/index';
+import { defaultDocumentNode } from './structure/defaultDocumentNode';
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,7 @@ export default defineConfig({
   projectId: 'hymq2lsg',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({ structure, defaultDocumentNode }), visionTool()],
 
   schema: {
     types: schemaTypes,
